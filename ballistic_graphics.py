c252de64e1972ae10c706ec0bcec8113930205c0
Olaf Seisler <olaf.seisler@gmail.com>
2021-09-25 19:42:01 +0300

    Refactored take_closest()

--- ballistic_graphics.py
+++ ballistic_graphics.py
@@ -3,6 +3,7 @@ import time as timer
 import matplotlib
 import matplotlib.pyplot as plt
 
+
 def draw_plot(data_x, data_y, x_name, y_name):
     fig, ax = plt.subplots()
     plt.plot(data_x, data_y)
@@ -12,9 +13,10 @@ def draw_plot(data_x, data_y, x_name, y_name):
     plt.ylim(bottom=0, top=plt.xlim()[1])
     plt.show()
 
+
 start = timer.perf_counter()
 aim_trajectory = main.findAngle(25000, 45, -10, 27.5, 0.01, 20)
 print("This process took", round(timer.perf_counter()-start, 6), "second(s)")
 
 aim_trajectory.print_data()
-main.draw_plot([x[0] for x  in aim_trajectory.pos_points], [x[1] for x in aim_trajectory.pos_points], "Distance (m)", "Altitude (m)")
+draw_plot([x[0] for x in aim_trajectory.pos_points], [x[1] for x in aim_trajectory.pos_points], "Distance (m)", "Altitude (m)")
